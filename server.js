6da5baf6d1cf92d1c41cdfff554fc634873dc22d
Md Abdulla Al Mamun <mamun.cseian@gmail.com>
2019-12-03 20:17:09 +0600

    Creating CRUD Operation using mongoose on node.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({path: './config.env'});
+//console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>', 
+    process.env.DATABASE_PASSWORD
+    );
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(con => {
+    console.log('DB connection successful!');
+});
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+
+});
